1f1fce5d3b7591ec819798f9a701d73777694bb9
Muhammad Farhan <muhammad.farhan@arpatech.com>
2023-02-21 03:33:01 +0500

    Update: Task using BackupAgent

--- app/src/main/java/com/example/demobackup/backup/MyBackupAgent.kt
+++ app/src/main/java/com/example/demobackup/backup/MyBackupAgent.kt
@@ -1,15 +1,41 @@
 package com.example.demobackup.backup
 
-import android.app.backup.BackupAgentHelper
-import android.app.backup.SharedPreferencesBackupHelper
-import com.example.demobackup.utils.Constant.PREFS_BACKUP_KEY
-import com.example.demobackup.utils.Constant.PREFS_FILENAME
+import android.app.backup.BackupAgent
+import android.app.backup.BackupDataInput
+import android.app.backup.BackupDataOutput
+import android.os.ParcelFileDescriptor
+import com.example.demobackup.utils.Constant.KEY_PREF_STRING
+import com.example.demobackup.utils.PrefKtx.getPref
+import com.example.demobackup.utils.PrefKtx.putAnyThing
 
-class MyBackupAgent : BackupAgentHelper() {
+class MyBackupAgent : BackupAgent() {
 
-    override fun onCreate() {
-        super.onCreate()
-        val prefsHelper = SharedPreferencesBackupHelper(this, PREFS_FILENAME)
-        addHelper(PREFS_BACKUP_KEY, prefsHelper)
+    override fun onBackup(
+        oldState: ParcelFileDescriptor?,
+        data: BackupDataOutput?,
+        newState: ParcelFileDescriptor?
+    ) {
+        // Save the string value to the backup
+        val myString = getPref()?.getString(KEY_PREF_STRING, null)
+        if (myString != null) {
+            val bytes = myString.toByteArray(Charsets.UTF_8)
+            data?.writeEntityHeader(KEY_PREF_STRING, bytes.size)
+            data?.writeEntityData(bytes, bytes.size)
+        }
+    }
+
+    override fun onRestore(data: BackupDataInput?, appVersionCode: Int, newState: ParcelFileDescriptor?) {
+        // Restore the string value from the backup
+        while (data?.readNextHeader() == true) {
+            val key = data.key
+            val dataSize = data.dataSize
+            if (key == KEY_PREF_STRING) {
+                val dataBytes = ByteArray(dataSize)
+                data.readEntityData(dataBytes, 0, dataSize)
+                val myString = String(dataBytes, Charsets.UTF_8)
+                getPref()?.putAnyThing(KEY_PREF_STRING,myString)
+            }
+            data.skipEntityData()
+        }
     }
 }
---
+++ app/src/main/java/com/example/demobackup/backup/MyBackupAgentHelper.kt
@@ -0,0 +1,15 @@
+package com.example.demobackup.backup
+
+import android.app.backup.BackupAgentHelper
+import android.app.backup.SharedPreferencesBackupHelper
+import com.example.demobackup.utils.Constant.PREFS_BACKUP_KEY
+import com.example.demobackup.utils.Constant.PREFS_FILENAME
+
+class MyBackupAgentHelper : BackupAgentHelper() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val prefsHelper = SharedPreferencesBackupHelper(this, PREFS_FILENAME)
+        addHelper(PREFS_BACKUP_KEY, prefsHelper)
+    }
+}
